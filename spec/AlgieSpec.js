15ffd573aa58ec7958d23f742288cede19363755
David Boureau <bdavidxyz@gmail.com>
2018-09-17 16:09:33 +0200

    improved

--- spec/AlgieSpec.js
+++ spec/AlgieSpec.js
@@ -1,20 +1,19 @@
 describe("Algie", function() {
 
-
-  describe("algie_allMultiplyBy", function() {
-    it("should convert [1,2,3] into [2,4,6] when all elements are multiplied by 2", function() {
-      expect(allMultiplyBy([1,2,3], 2)).toEqual([2,4,6]);
-    });
-    it("should convert [3,7,9] into [9,21,27] when all elements are multiplied by 3", function() {
-      expect(allMultiplyBy([3,7,9], 3)).toEqual([9,21,27]);
-    });
-
-  });
-
   describe("algie_42", function() {
     it("should return 42", function() {
       expect(algie_42()).toEqual(42);
     });
   });
+
+  describe("algie_allMultiplyBy", function() {
+    it("should convert [1, 2, 3] into [2, 4, 6] when all elements are multiplied by 2", function() {
+      expect(allMultiplyBy([1, 2, 3], 2)).toEqual([2, 4, 6]);
+    });
+    it("should convert [3, 7, 9, 11] into [9, 21, 27, 33] when all elements are multiplied by 3", function() {
+      expect(allMultiplyBy([3, 7, 9, 11], 3)).toEqual([9, 21, 27, 33]);
+    });
+  });
+
 
 });
--- src/Algie.js
+++ src/Algie.js
@@ -1,7 +1,8 @@
-// algie_allMultiplyBy = function(array, number) {
-// };
+algie_42 = function() {
+  // This already works, but this is more a basic example than something you would encounter on real projects.
+  return 42;
+};
 
-// algie_42 = function() {
-//   return 42;
-// };
-
+algie_allMultiplyBy =  function(array, number) {
+  // Write function here
+}
