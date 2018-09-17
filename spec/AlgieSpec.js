8dad20e01457c45ea77c2339f95d45fee4523adb
David Boureau <bdavidxyz@gmail.com>
2018-09-17 16:31:21 +0200

    better

--- spec/AlgieSpec.js
+++ spec/AlgieSpec.js
@@ -8,10 +8,10 @@ describe("Algie", function() {
 
   describe("algie_allMultiplyBy", function() {
     it("should convert [1, 2, 3] into [2, 4, 6] when all elements are multiplied by 2", function() {
-      expect(allMultiplyBy([1, 2, 3], 2)).toEqual([2, 4, 6]);
+      expect(algie_allMultiplyBy([1, 2, 3], 2)).toEqual([2, 4, 6]);
     });
     it("should convert [3, 7, 9, 11] into [9, 21, 27, 33] when all elements are multiplied by 3", function() {
-      expect(allMultiplyBy([3, 7, 9, 11], 3)).toEqual([9, 21, 27, 33]);
+      expect(algie_allMultiplyBy([3, 7, 9, 11], 3)).toEqual([9, 21, 27, 33]);
     });
   });
 
