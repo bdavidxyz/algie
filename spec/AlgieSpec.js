e85ad2186d300cdb3def47ac100e5936c5d189b0
David Boureau <bdavidxyz@gmail.com>
2018-09-17 16:44:14 +0200

    algie_insertEverywhere

--- spec/AlgieSpec.js
+++ spec/AlgieSpec.js
@@ -15,5 +15,47 @@ describe("Algie", function() {
     });
   });
 
+  describe("algie_multiplyEach", function() {
+    it("[1, 2, 3] multiplied by [1, 2, 3] gives [1, 4, 9]", function() {
+      expect(algie_multiplyEach([1, 2, 3], [1, 2, 3])).toEqual([1, 4, 9]);
+    });
+    it("[2, 2, 2] multiplied by [1, 2, 3] gives [2, 4, 6]", function() {
+      expect(algie_multiplyEach([2, 2, 2], [2, 4, 6])).toEqual([2, 4, 6]);
+    });
+    it("[9, 41] multiplied by [6, 76] gives [54, 3116]", function() {
+      expect(algie_multiplyEach([9, 41], [6, 76])).toEqual([54, 3116]);
+    });
+  });
+
+  describe("algie_completeWith", function() {
+    it("[1, 2, 3] must have a size of 5. If not, complete with 42", function() {
+      expect(algie_completeWith([1, 2, 3], 5, 42)).toEqual([1, 2, 3, 42, 42]);
+    });
+    it("[4, 0, -3] must have a size of 8. If not, complete with boolean false", function() {
+      expect(algie_completeWith([4, 0, -3], 8, false)).toEqual([4, 0, -3, false, false, false, false, false]);
+    });
+  });
+
+  describe("algie_elementNext", function() {
+    it("element next to 9 in array [8, 9, 33] is 33", function() {
+      expect(algie_elementNext([8, 9, 33], 9)).toEqual(33);
+    });
+    it("element next to 42 in array [8, 9, 33, 42, 78, 13] is 78", function() {
+      expect(algie_elementNext([8, 9, 33, 42, 78, 13], 42)).toEqual(78);
+    });
+    it("element next to 13 in array [8, 9, 33, 42, 78, 13] is 8 (yes ! element after the last one is the first one)", function() {
+      expect(algie_elementNext([8, 9, 33, 42, 78, 13], 42)).toEqual(78);
+    });
+  });
+
+  describe("algie_insertEverywhere", function() {
+    it("'me' with 'b' everywhere will become 'bmbeb'", function() {
+      expect(algie_insertEverywhere('foo', 'A')).toEqual('AfAoAoA');
+    });
+    it("'foo' with 'A' everywhere will become 'AfAoAoA'", function() {
+      expect(algie_insertEverywhere('foo', 'A')).toEqual('AfAoAoA');
+    });
+  });
+
 
 });
